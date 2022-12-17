7fadbdd1dd5b70ec4715deb79e0f2cfe422315de
Maria Roman <120798790+mariiamonk@users.noreply.github.com>
2022-12-17 18:46:24 +0300

    Create lab4.h

---
+++ lab4/lab4.2/lab4.h
@@ -0,0 +1,12 @@
+#ifndef LAB4
+#define LAB4
+
+size_t mystrlen(const char *s);
+char* myreadline(const char *prompt);
+char* mystrcpy(char* str, const char* ptr);
+char* mystrtok(char* str, const char* ptr);
+char *task(const char *str);
+int checkline(char* str);
+char* mystrchr (const char *str, int ch);
+
+#endif
