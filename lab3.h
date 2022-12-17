085c393d364b057f90a69a9a7eddc8b891feeb10
Maria Roman <120798790+mariiamonk@users.noreply.github.com>
2022-12-17 18:27:40 +0300

    Create lab3.h

---
+++ lab3.h
@@ -0,0 +1,13 @@
+#ifndef LAB3
+#define LAB3
+
+double* enterArr(int *len);
+void printArr(double *arr, int len);
+double* addElemArr(double *arr, int *len, double elem, int pos);
+double* DelElemArr(double *arr, int *len, int size, int pos);
+double factPart(double x);
+double* task(double *arr, int *len, int *size, int N);
+int checkValuesInt(int a);
+int checkValuesDouble(double *b);
+
+#endif
